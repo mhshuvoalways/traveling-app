3e69f73de012d218042c7da73dc3161ef45d9b82
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-08-02 22:36:13 +0600

    map

--- client/components/map/index.jsx
+++ client/components/map/index.jsx
@@ -1,9 +1,10 @@
+
 const index = ({ cardHover }) => {
   return (
     <div className="w-full md:w-5/12">
       {cardHover === 1 && (
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111643.70464485248!2d-80.96486949219151!3d29.020963517258767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7292b5700b65b%3A0xc1bb3239363bc87c!2sNew%20Smyrna%20Beach%2C%20FL%2C%20USA!5e0!3m2!1sen!2sbd!4v1690303903691!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3490.2973570993436!2d-80.8589032!3d28.978558300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e72c575edee729%3A0x9bfd33ce783d5d1!2s4849%20Saxon%20Dr%2C%20New%20Smyrna%20Beach%2C%20FL%2032169%2C%20USA!5e0!3m2!1sen!2sbd!4v1690993961320!5m2!1sen!2sbd"
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
@@ -12,7 +13,7 @@ const index = ({ cardHover }) => {
       )}
       {cardHover === 2 && (
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111701.42412635511!2d-80.95816262767246!3d28.967525608583124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7189df1428a2d%3A0xab43b4b915a75d3d!2sEdgewater%2C%20FL%2C%20USA!5e0!3m2!1sen!2sbd!4v1690304086604!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3489.3012433872223!2d-80.8778822!3d29.008070299999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e72c0cbd660b25%3A0xb93809e42fdce219!2s3801%20S%20Atlantic%20Ave%2C%20New%20Smyrna%20Beach%2C%20FL%2032169%2C%20USA!5e0!3m2!1sen!2sbd!4v1690994063738!5m2!1sen!2sbd"
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
@@ -21,7 +22,7 @@ const index = ({ cardHover }) => {
       )}
       {cardHover === 3 && (
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111701.42412635511!2d-80.95816262767246!3d28.967525608583124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7189df1428a2d%3A0xab43b4b915a75d3d!2sEdgewater%2C%20FL%2C%20USA!5e0!3m2!1sen!2sbd!4v1690304086604!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1745.1803281206019!2d-80.8594633!3d28.976682!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e72c56e2033569%3A0x1c10935ba58e3e39!2s5300%20S%20Atlantic%20Ave%2C%20New%20Smyrna%20Beach%2C%20FL%2032169%2C%20USA!5e0!3m2!1sen!2sbd!4v1690994102886!5m2!1sen!2sbd"
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
