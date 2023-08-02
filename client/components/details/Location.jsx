2be7a44a7ba5b5f5705bae76aaca20e676ed7d27
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-08-02 23:03:53 +0600

    not pet friendly

--- client/components/details/Location.jsx
+++ client/components/details/Location.jsx
@@ -4,7 +4,7 @@ const Location = ({ data }) => {
   return (
     <div className="shadow-sm border rounded-xl p-5 md:p-10 bg-white">
       <p className="text-3xl text-gray-800 font-semibold mb-2">Location</p>
-      <p>{data?.itemLocation.placeName}</p>
+      <p className="mb-5">{data?.itemLocation.placeName}</p>
       {data?.id === 1 && (
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3490.2973570993436!2d-80.8589032!3d28.978558300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e72c575edee729%3A0x9bfd33ce783d5d1!2s4849%20Saxon%20Dr%2C%20New%20Smyrna%20Beach%2C%20FL%2032169%2C%20USA!5e0!3m2!1sen!2sbd!4v1690993961320!5m2!1sen!2sbd"
